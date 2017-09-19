c10533b978539ee57fe9a79003846dbb955f5a70
Graham Cooper <graham.cooper33@gmail.com>
2017-09-18 23:31:02 -0400

    Moved to elixir 1.4 to allow build packs to work on heroku

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Fioria.Mixfile do
     [
       app: :fioria,
       version: "0.0.1",
-      elixir: "~> 1.5",
+      elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
       start_permanent: Mix.env == :prod,
