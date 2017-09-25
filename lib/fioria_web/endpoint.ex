3a525377bca0714399e6ee09b520f4c4b7525c84
Graham Cooper <graham.cooper33@gmail.com>
2017-09-24 23:22:46 -0400

    Added https

--- lib/fioria_web/endpoint.ex
+++ lib/fioria_web/endpoint.ex
@@ -49,7 +49,7 @@ defmodule FioriaWeb.Endpoint do
   def init(_key, config) do
     if config[:load_from_system_env] do
       port = System.get_env("PORT") || raise "expected the PORT environment variable to be set"
-      {:ok, Keyword.put(config, :http, [:inet6, port: port])}
+      {:ok, Keyword.put(config, :https, [:inet6, port: port])}
     else
       {:ok, config}
     end
